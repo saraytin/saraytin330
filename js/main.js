fa2d419bd41bdb210c65e6ab105f5bf504754480
saraytin <smee.tindall@gmail.com>
2021-05-08 21:32:59 -0500

    week3 notes

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
 const links = [
-    {
-      label: "Week 1 Notes",
-      url: "week1/index.html"
-    },
-    {
-      label: "Week 2 Notes",
-      url: "week2/index.html"
-    }
+	{
+		label: 'Week 1 Notes',
+		url: 'week1/',
+	},
+	{
+		label: 'Week 2 Notes',
+		url: 'week2/',
+	},
+	{
+		label: 'Week 3 Notes',
+		url: 'week3/',
+	},
 ];
-  
+
 buildList();
 
 function buildList() {
-    var card = document.createElement('section');
-    var list = document.createElement('ol');
-    for (i = 0; i < links.length; i++) {
-        let item = document.createElement('li');
-        let link = document.createElement('a');
-        link.setAttribute('href', links[i].url);
-        link.textContent = links[i].label;
-        item.appendChild(link);
-        list.appendChild(item);
-    }
-    card.appendChild(list);
-    document.querySelector('div#list').appendChild(card);
+	var card = document.createElement('section');
+	var list = document.createElement('ol');
+	for (i = 0; i < links.length; i++) {
+		let item = document.createElement('li');
+		let link = document.createElement('a');
+		link.setAttribute('href', links[i].url);
+		link.textContent = links[i].label;
+		item.appendChild(link);
+		list.appendChild(item);
+	}
+	card.appendChild(list);
+	document.querySelector('div#list').appendChild(card);
 }
