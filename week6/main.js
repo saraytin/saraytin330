e37d5d34ad88474c707cc45999e894f5dd778e34
saraytin <smee.tindall@gmail.com>
2021-06-01 20:32:15 -0500

    remove not working, unstyled

--- week6/main.js
+++ week6/main.js
@@ -1,19 +1,42 @@
 import Todo from './modules/Todos.js';
-import { renderTodo, addTodo, checkTask } from './modules/utilities.js';
+import { renderTodo, addTodo, checkTask, startList } from './modules/utilities.js';
 import { getList, setList } from './modules/ls.js';
 
-let todoList = getList();
-switch (todoList) {
+document.getElementById('btnsave').addEventListener('click', addTodo, false);
+checkTask();
+
+let todoList = [];
+try {
+  todoList = getList();
+} catch (err) {
+  startList();
+}
+/* switch (todoList) {
   case '':
   case null:
     todoList = [];
     break;
   default:
-    renderTodo();
+    renderTodo(todoList);
+} */
+
+renderTodo(todoList);
+
+/* function addTodo() {
+  let task = document.querySelector('textarea#newTodo').value;
+  todoList.push(task);
+  document.querySelector('textarea#newTodo').value = '';
+  renderTodo();
+  setList(todoList);
+} */
+
+export function saveTodo(text) {
+  let task = new Todo(text);
+  todoList.push(task);
+  renderTodo(todoList);
+  setList(todoList);
 }
 
-function saveTodo(task) {
-  todoList.append(task);
+export function removeTodo() {
+  console.log('task cannot be removed');
 }
-
-setList();
--- week6/modules/utilities.js
+++ week6/modules/utilities.js
@@ -1,9 +1,14 @@
 // Document manipulation
-const removeUrl = '../images/remove.png';
+import { saveTodo, removeTodo } from '../main.js';
+
+const removeUrl = './images/remove.png';
 const removeAlt = 'picture of a red x';
 
 export function renderTodo(todoList) {
   /*  let todoJson = JSON.parse(todoList); */
+  let i = 0;
+  console.log(todoList);
+  document.getElementById('list').innerHTML = '';
   for (i = 0; i < todoList.length; i++) {
     let card = document.createElement('section');
     let checkbox = document.createElement('input');
@@ -13,25 +18,49 @@ export function renderTodo(todoList) {
     checkbox.setAttribute('id', 'item' + i);
     checkbox.setAttribute('name', 'item' + i);
     label.setAttribute('for', 'item' + i);
-    label.textContent(todoJson[i].task);
-    if (todoJson[i].done) {
+    label.setAttribute('id', 'item' + i + '-label');
+    label.textContent = todoList[i].task;
+    if (todoList[i].done) {
       checkbox.checked = true;
-      label.setAttribute('style', 'text-decoration:strikethrough;');
     }
     remove.setAttribute('src', removeUrl);
     remove.setAttribute('alt', removeAlt);
+    remove.setAttribute('class', 'remove');
     card.appendChild(checkbox);
     card.appendChild(label);
     card.appendChild(remove);
-    document.querySelector('div.list').appendChild(card);
+    document.querySelector('div#list').appendChild(card);
   }
+
+  let divs1 = document.querySelectorAll('.remove');
+  divs1.forEach((el) => el.addEventListener('click', removeTodo, false));
+  let divs2 = document.querySelectorAll('input');
+  divs2.forEach((el) => el.addEventListener('click', checkTask, false));
 }
 
 export function addTodo() {
   let task = document.querySelector('textarea#newTodo').value;
   saveTodo(task);
   document.querySelector('textarea#newTodo').value = '';
-  renderTodo();
 }
 
-export function checkTask() {}
+export function checkTask() {
+  let inputs = document.querySelectorAll('input');
+  let labels = document.querySelectorAll('label');
+  let i = 0;
+  for (i = 0; i < inputs.length; i++) {
+    switch (inputs[i].checked) {
+      case true:
+        labels[i].setAttribute('style', 'text-decoration:line-through;');
+        break;
+      default:
+        labels[i].setAttribute('style', 'text-decoration:none;');
+    }
+  }
+}
+
+export function startList() {
+  let p = document.createElement('p');
+  p.textContent = 'Please add a task to start a to-do list.';
+  document.querySelector('div#list').appendChild(p);
+}
