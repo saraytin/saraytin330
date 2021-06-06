1d915ee2bc0a9f85d84862e7249379eb0525e88e
saraytin <smee.tindall@gmail.com>
2021-06-05 21:03:38 -0500

    week 7 notes

--- js/main.js
+++ js/main.js
@@ -1,43 +1,47 @@
 const links = [
   {
-    label: 'Week 1 Notes',
-    url: 'week1/',
+    label: "Week 1 Notes",
+    url: "week1/",
   },
   {
-    label: 'Week 2 Notes',
-    url: 'week2/',
+    label: "Week 2 Notes",
+    url: "week2/",
   },
   {
-    label: 'Week 3 Notes',
-    url: 'week3/',
+    label: "Week 3 Notes",
+    url: "week3/",
   },
   {
-    label: 'Week 4 Notes',
-    url: 'week4/',
+    label: "Week 4 Notes",
+    url: "week4/",
   },
   {
-    label: 'Week 5 Notes',
-    url: 'week5/',
+    label: "Week 5 Notes",
+    url: "week5/",
   },
   {
-    label: 'Week 6: ToDo List',
-    url: 'week6/',
+    label: "Week 6: ToDo List",
+    url: "week6/",
+  },
+  {
+    label: "Week 7 Notes",
+    url: "week7/",
   },
 ];
 
 buildList();
 
 function buildList() {
-  var card = document.createElement('section');
-  var list = document.createElement('ol');
+  var card = document.createElement("section");
+  var list = document.createElement("ol");
   for (i = 0; i < links.length; i++) {
-    let item = document.createElement('li');
-    let link = document.createElement('a');
-    link.setAttribute('href', links[i].url);
+    let item = document.createElement("li");
+    let link = document.createElement("a");
+    link.setAttribute("href", links[i].url);
     link.textContent = links[i].label;
     item.appendChild(link);
     list.appendChild(item);
   }
   card.appendChild(list);
-  document.querySelector('div#list').appendChild(card);
+  document.querySelector("div#list").appendChild(card);
 }
